16e1d5acdc5a93c0f984921cc0d65cbf2f8db155
Mario Càllisto <mario.callisto@gmail.com>
2025-09-18 17:19:52 +0200

    Geometry simplified again

--- src/main/scala/io/github/scala_tessella/editor/utils/Geometry.scala
+++ src/main/scala/io/github/scala_tessella/editor/utils/Geometry.scala
@@ -20,9 +20,6 @@ object Geometry:
      */
     val TAU: Radian = Radian(6.283185307179586)
     val TAU_2: Radian = Radian(Math.PI)
-    val TAU_3: Radian = TAU / 3
-    val TAU_4: Radian = Radian(1.5707963267948966)
-    val TAU_6: Radian = TAU_2 / 3
 
   extension (r: Radian)
 
@@ -54,7 +51,7 @@ object Geometry:
       Point(this.x + that.x, this.y + that.y)
 
     /** Difference of two points */
-    def minus(that: Point): Point =
+    private def minus(that: Point): Point =
       Point(this.x - that.x, this.y - that.y)
 
     def scale(factor: Double): Point =
@@ -67,20 +64,20 @@ object Geometry:
         Math.sin(theta)
       Point(x * cot - y * sit, x * sit + y * cot)
 
-    /** New point moved by polar coordinates
-     *
-     * @param rho   distance
-     * @param theta angle
-     */
-    def plusPolar(rho: Double)(theta: Radian): Point =
-      plus(Point.createPolar(rho, theta))
+//    /** New point moved by polar coordinates
+//     *
+//     * @param rho   distance
+//     * @param theta angle
+//     */
+//    def plusPolar(rho: Double)(theta: Radian): Point =
+//      plus(Point.createPolar(rho, theta))
 
-    /** New point moved by distance 1.0 */
-    def plusPolarUnit: Radian => Point =
-      plusPolar(1)
+//    /** New point moved by distance 1.0 */
+//    def plusPolarUnit: Radian => Point =
+//      plusPolar(1)
 
     /** Calculates the horizontal angle between two points */
-    def angleTo(other: Point): Radian =
+    private def angleTo(other: Point): Radian =
       LineSegment(this, other).horizontalAngle
 
     def distanceTo(other: Point): Double =
@@ -90,20 +87,19 @@ object Geometry:
     def alignWithStart(first: Point, second: Point): Point =
       minus(first).rotate(Radian.TAU - first.angleTo(second))
 
-    /** New point flipped vertically around the x-axis */
-    def flipVertically: Point =
-      Point(x, -y)
+//    /** New point flipped vertically around the x-axis */
+//    def flipVertically: Point =
+//      Point(x, -y)
 
-
-  object Point:
-
-    /** Creates a point at origin */
-    def apply(): Point =
-      Point(0, 0)
-
-    /** Creates a point from polar coordinates */
-    def createPolar(rho: Double, theta: Radian): Point =
-      Point(rho * Math.cos(theta), rho * Math.sin(theta))
+//  object Point:
+//
+//    /** Creates a point at origin */
+//    def apply(): Point =
+//      Point(0, 0)
+//
+//    /** Creates a point from polar coordinates */
+//    def createPolar(rho: Double, theta: Radian): Point =
+//      Point(rho * Math.cos(theta), rho * Math.sin(theta))
 
   /** Line segment, defined as the set of points located between the two end points. */
   case class LineSegment(point1: Point, point2: Point):
