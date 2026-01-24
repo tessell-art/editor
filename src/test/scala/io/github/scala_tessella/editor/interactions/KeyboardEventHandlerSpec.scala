b0af53a49f8b0a8e427649ddc81b65127cc3a940
Mario Càllisto <mario.callisto@gmail.com>
2026-01-24 22:49:17 +0100

    Cleaner

--- src/test/scala/io/github/scala_tessella/editor/interactions/KeyboardEventHandlerSpec.scala
+++ src/test/scala/io/github/scala_tessella/editor/interactions/KeyboardEventHandlerSpec.scala
@@ -4,23 +4,21 @@ import munit.FunSuite
 
 class KeyboardEventHandlerSpec extends FunSuite:
 
-  test("rotationDeltaForKey maps rotation keys") {
+  test("rotationDeltaForKey maps rotation keys"):
     assertEquals(KeyboardEventHandler.rotationDeltaForKey("r"), Some(15))
     assertEquals(KeyboardEventHandler.rotationDeltaForKey("R"), Some(15))
     assertEquals(KeyboardEventHandler.rotationDeltaForKey("e"), Some(-15))
     assertEquals(KeyboardEventHandler.rotationDeltaForKey("E"), Some(-15))
     assertEquals(KeyboardEventHandler.rotationDeltaForKey("x"), None)
-  }
 
-  test("zoomFactorForKey maps zoom keys") {
+  test("zoomFactorForKey maps zoom keys"):
     assertEquals(KeyboardEventHandler.zoomFactorForKey("+"), Some(1.1))
     assertEquals(KeyboardEventHandler.zoomFactorForKey("="), Some(1.1))
     assertEquals(KeyboardEventHandler.zoomFactorForKey("-"), Some(1.0 / 1.1))
     assertEquals(KeyboardEventHandler.zoomFactorForKey("_"), Some(1.0 / 1.1))
     assertEquals(KeyboardEventHandler.zoomFactorForKey("z"), None)
-  }
 
-  test("undo/redo/save shortcut helpers detect modifiers") {
+  test("undo/redo/save shortcut helpers detect modifiers"):
     assert(KeyboardEventHandler.isUndoShortcut("z", primary = true, shift = false))
     assert(!KeyboardEventHandler.isUndoShortcut("z", primary = true, shift = true))
     assert(!KeyboardEventHandler.isUndoShortcut("z", primary = false, shift = false))
@@ -31,4 +29,3 @@ class KeyboardEventHandlerSpec extends FunSuite:
 
     assert(KeyboardEventHandler.isSaveShortcut("s", primary = true))
     assert(!KeyboardEventHandler.isSaveShortcut("s", primary = false))
-  }
