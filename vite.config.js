0293c75684373fd37c82a4247e001432fda103f3
Mario Càllisto <mario.callisto@gmail.com>
2026-04-24 12:01:34 +0200

    Asset URLs become relative

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 
 export default defineConfig({
+    base: './',
     plugins: [scalaJSPlugin()],
     optimizeDeps: {
         include: [
